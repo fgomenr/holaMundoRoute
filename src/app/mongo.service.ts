72801c5c42d0a339767b85c131164602a89992e1
Félix Gómez Enríquez <felixmihmo@gmail.com>
2023-03-10 10:47:48 +0100

    Subida

--- src/app/mongo.service.ts
+++ src/app/mongo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,14 +14,15 @@ export class MongoService {
 
   insertarBBDD(body:any): Observable<any> {
     console.log(body);
-    const headers = new HttpHeaders();   //BUSCAR INFO DE ESTO
-    headers.set('Content-Type', 'application/json; charset=utf-8');
-    headers.set()
-    let post=this.httpClient.post('http://127.0.0.1:3000/insertar',body, {headers: headers});
+    const httpOptions = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'http://localhost:4200'
+      })
+    };
+
+    let post=this.httpClient.post('http://127.0.0.1:3000/insertar',body, httpOptions);
     console.log(post);
-    setTimeout(() => {
-      
-    }, 10000000);
+   
     return post;
   }
 }
